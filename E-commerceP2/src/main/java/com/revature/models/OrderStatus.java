e629b32ca0a1547a5bea78c68caa429f6412e11b
Ralphomines <raphael.kiarie@yahoo.com>
2022-09-15 16:54:00 -0600

    order models

--- E-commerceP2/src/main/java/com/revature/models/OrderStatus.java
+++ E-commerceP2/src/main/java/com/revature/models/OrderStatus.java
@@ -1,4 +1,24 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "order_status")
 public class OrderStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_status_id")
+    private int orderStatusID;
+
+    @Column(name = "order_status", nullable = false)
+    private String orderStatus;
+
 }
