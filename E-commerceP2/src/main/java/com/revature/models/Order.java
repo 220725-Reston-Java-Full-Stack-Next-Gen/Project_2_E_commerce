b73517b4ef7f7196fa620c78762da1743525f1c5
Ralphomines <raphael.kiarie@yahoo.com>
2022-09-16 13:12:13 -0600

    added orders models

--- E-commerceP2/src/main/java/com/revature/models/Order.java
+++ E-commerceP2/src/main/java/com/revature/models/Order.java
@@ -1,4 +1,48 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderID;
+
+    @Column(name = "quantity", nullable = false)
+    private int orderQuantity;
+
+    @Column(name = "total_price", nullable = false)
+    private double orderTotalPrice;
+
+    @Column(name = "date_created", nullable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(name = "date_shipped")
+    private LocalDateTime dateShipped;
+
+    @Column(name = "date_modified")
+    private LocalDateTime dateModified;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_user_id", referencedColumnName = "user_id")
+    private User orderOwner;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_payment_id", referencedColumnName = "payment_id")
+    private Payment payment;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_status_id", referencedColumnName = "order_status_id")
+    private OrderStatus orderStatus;
+
 }
---
+++ E-commerceP2/src/main/java/com/revature/models/OrderDetails.java
@@ -0,0 +1,44 @@
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "order_details")
+public class OrderDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_details_id")
+    private int orderDetailsID;
+
+    @Column(name = "order_item_number", nullable = false)
+    private int orderItemNumber;
+
+    @Column(name = "unit_price", nullable = false)
+    private double unitPrice;
+
+    @Column(name = "product_quantity", nullable = false)
+    private int productQuantity;
+
+    @Column(name = "date_created", nullable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(name = "date_modified")
+    private LocalDateTime dateModified;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id", referencedColumnName = "order_id")
+    private Order order;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
+    private Product product;
+}
