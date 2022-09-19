6b5cabf2741cc0f1d8fc0674e881fde0b1c1c363
Ralphomines <raphael.kiarie@yahoo.com>
2022-09-19 15:34:09 -0600

    added models

---
+++ E-commerceP2/src/main/java/com/revature/models/Cart.java
@@ -0,0 +1,36 @@
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "cart")
+
+public class Cart {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private int cartID;
+    
+    @Column(name = "order_quantity", nullable = false)
+    private int orderQuantity;
+    
+    @Column(name = "date_created")
+    private LocalDateTime dateCreated;
+
+    @Column(name = "date_modified")
+    private LocalDateTime dateModified;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User userID;
+
+}
