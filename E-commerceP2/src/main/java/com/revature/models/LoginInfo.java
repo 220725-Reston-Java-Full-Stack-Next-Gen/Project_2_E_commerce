b1c644a137209d6d3140f03428ba6d7ad9c59ced
Ralphomines <raphael.kiarie@yahoo.com>
2022-09-19 15:37:44 -0600

    added LoginInfo and LoginRepo

---
+++ E-commerceP2/src/main/java/com/revature/models/LoginInfo.java
@@ -0,0 +1,30 @@
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "login_log")
+public class LoginInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "login_log_id")
+    private int loginLogID;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @Column(name = "login_time", nullable = false)
+    private LocalDateTime loginTime;
+
+    @Column(name = "logout_time")
+    private LocalDateTime logoutTime;
+}
