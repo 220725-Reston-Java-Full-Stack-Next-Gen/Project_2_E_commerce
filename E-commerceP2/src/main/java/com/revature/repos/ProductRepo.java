b1b2f5b39e595f8e5c1981abfd0b198a517b78f4
brucejones43 <sundberg43@gmail.com>
2022-09-19 14:44:11 -0700

    update product repo

--- E-commerceP2/src/main/java/com/revature/repos/ProductRepo.java
+++ E-commerceP2/src/main/java/com/revature/repos/ProductRepo.java
@@ -18,6 +18,16 @@ public interface ProductRepo extends JpaRepository<Product, Integer> {
     @Query(value="SELECT * FROM products JOIN product_type_id ON product_type_id WHERE product_type=?1")
     public boolean findProductByType(String type);
 
-
+    // crud operations on products
+//
+//    @Query(value="SELECT * FROM products where product_id=?1")
+//    public boolean getProductById(int product_id);
+//
+//    @Query(value="SELECT * FROM products where product_id=?1")
+//    public boolean getProductById(int product_id);
+//
+//
+//    @Query(value="update products SET ")
+//    public boolean getProductById(int product_id);
 
 }
