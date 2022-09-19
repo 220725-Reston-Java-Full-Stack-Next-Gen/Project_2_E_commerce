39c4e51687531b7ff3e8b594690bb319f82ae7fd
brucejones43 <sundberg43@gmail.com>
2022-09-19 14:27:43 -0700

    product repo

--- E-commerceP2/src/main/java/com/revature/repos/ProductRepo.java
+++ E-commerceP2/src/main/java/com/revature/repos/ProductRepo.java
@@ -1,4 +1,21 @@
 package com.revature.repos;
 
-public interface ProductRepo {
+import com.revature.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+    @Query(value="SELECT * FROM products JOIN product_brand_id ON product_brand_id WHERE product_brand=?1")
+    public boolean findProductByBrand(String brand);
+    @Query(value="SELECT * FROM products JOIN product_type_id ON product_type_id WHERE product_type=?1")
+    public boolean findProductByType(String type);
+
+
+
 }
