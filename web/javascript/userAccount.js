3322eed2a414ea2257265f0dfa9f68730916c939
Varada K <37877180+vklane@users.noreply.github.com>
2022-09-21 17:16:03 -0400

    Delete userAccount.js

--- web/javascript/userAccount.js
+++
@@ -1,6 +0,0 @@
-var loggedInUser = JSON.parse(sessionStorage.getItem("loggedInUser"));
-console.log(loggedInUser);
-
-var firstname = document.getElementById("firstname");
-firstname.value = loggedInUser.firstName;
-
