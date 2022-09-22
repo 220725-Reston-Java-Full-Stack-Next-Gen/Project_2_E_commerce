db7901d892e06f73b899d093efbe47463b6d656e
Ralphomines <raphael.kiarie@yahoo.com>
2022-09-22 11:10:48 -0600

    Cart Repo Error updated

--- E-commerceP2/src/main/java/com/revature/repos/CartRepo.java
+++ E-commerceP2/src/main/java/com/revature/repos/CartRepo.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import com.revature.models.Cart;
 import com.revature.models.User;
 
+import java.util.List;
+
 public interface CartRepo extends JpaRepository<Cart, Integer> {
 	
 	@Query(value = "INSERT INTO cart (cartID) VALUES (DEFAULT)", nativeQuery = true)
 	public Cart saveNew(Cart cart);	
 	
 	@Query(value = "SELECT * FROM cart WHERE userID = ?1", nativeQuery = true)
-	public Cart getCart(User user);	
+	public List<Cart> getCart(int userID);
 	
 	@Query(value = "INSERT INTO cart SELECT product_id=?1 FROM product", nativeQuery = true)
 	boolean save(int productID);
